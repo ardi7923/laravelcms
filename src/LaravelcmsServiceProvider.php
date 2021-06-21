837f9bbec8a8ad2e6a8aa9576eb6bcee00472e59
ardi7923 <ardiirawan777@gmail.com>
2021-06-21 22:57:29 +0800

    upload translation

---
+++ src/LaravelcmsServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Ardi7923\Laravelcms;
+
+use Illuminate\Support\ServiceProvider;
+
+class Laravel extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Boot the instance, add macros for datatable engines.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadTranslationsFrom(__DIR__.'/../transations', 'courier');
+    }
+}
