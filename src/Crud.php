1dd706b4c47f13b2c81eb590d056bc7c3f03202c
ardi7923 <ardiirawan777@gmail.com>
2024-01-04 01:03:19 +0800

    bugfixing crud

--- src/Crud.php
+++ src/Crud.php
@@ -7,5 +7,22 @@ use Ardi7923\Laravelcms\Utilities\RequestUtility;
 
 class Crud extends Controller
 {
-  use RequestUtility;
+    use RequestUtility;
+
+    protected $model;
+    protected $folder;
+    protected $url;
+    protected $validator;
+    
+
+    public function __construct()
+    {
+        $this->model = new $this->model;
+        $this->request = app('request');
+        if($this->validator){
+            $this->validator = new $this->validator;
+        }
+
+        
+    }
 }
